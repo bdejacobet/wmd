babdaa77e12606e711cd1ebf9a3b2c1cad456c0f
Benoit de Jacobet <benijaco@gmail.com>
2013-05-06 18:04:45 +0200

    mise en place formulaire

---
+++ src/Wmd/WatchMyDeskBundle/Form/Type/DeskType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Wmd\WatchMyDeskBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+//use Wmd\WatchMyDeskBundle\Form\Type\DeskPictureType;
+
+class DeskType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('title')
+            ->add('summary')
+            ->add('description');
+    }
+
+    public function getDefaultOptions(array $options)
+    {
+        return array(
+            'data_class' => 'Wmd\WatchMyDeskBundle\Entity\Desk',
+        );
+    }
+
+    public function getName()
+    {
+        return 'Desk';
+    }
+}
